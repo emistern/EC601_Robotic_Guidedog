d1d5a73dafb49e4b05df1c4c6796a87efe3c75a2
Emily Stern <emistern@bu.edu>
2018-10-13 14:20:06 -0400

    Added ability to load in a numpy array

--- wall_detection/discretize_map.py
+++ wall_detection/discretize_map.py
@@ -1,2 +1,17 @@
 #Copyright Emily Stern emistern@bu.edu
 #This file will import a numpy matrices and then discretize each matrix based on ranges of data.
+
+#Import numpy array
+import numpy as np
+
+
+#Load in one depth data matrix
+frame1_depth_data = np.load('depth0000.npy')
+
+
+print(np.unique(frame1_depth_data))
+
+# Potentially real sense library could show us what the conversion is between actual distance
+# the values in the depth matrix. 
+
+# Start with three ranges of data to look into
