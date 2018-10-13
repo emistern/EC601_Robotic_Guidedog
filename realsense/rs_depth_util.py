ffb4301556d364887fc0e0bab46084b47d37bac3
yxiao1996 <xiaoyu960522@sina.com>
2018-10-13 15:00:26 +0800

    add fitering tools to depth image worker

--- realsense/rs_depth_util.py
+++ realsense/rs_depth_util.py
@@ -70,8 +70,82 @@ class depth_worker():
 
             input()
 
+    def play_depth_stream(self, bagfile_dir):
 
-    def save_depth_to_npy(self, bagfile_dir, n_img = 3, interval = 100000):
+        # -------------------------------------------- #
+        # Play bag file depth images with filtering
+        # -------------------------------------------- #
+
+        import pyrealsense2 as rs
+
+        input = bagfile_dir
+
+        try:
+            # Create pipeline
+            pipeline = rs.pipeline()
+
+            # Create a config object
+            config = rs.config()
+            
+            # Tell config that we will use a recorded device from filem to be used by the pipeline through playback.
+            rs.config.enable_device_from_file(config, input)
+            
+            # Configure the pipeline to stream the depth stream
+            config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+
+            # Start streaming from file
+            pipeline.start(config)
+
+            # Declare filters
+            dec_filter = rs.decimation_filter()   # Decimation - reduces depth frame density
+            
+            spat_filter = rs.spatial_filter()          # Spatial    - edge-preserving spatial smoothing
+            
+            temp_filter = rs.temporal_filter()    # Temporal   - reduces temporal noise
+
+            fill_filter = rs.hole_filling_filter()  # Hole Filling filter
+
+            disp_filter = rs.disparity_transform()  # Disparity Transform filter
+
+            # Streaming loop
+            while True:
+                # Get frameset of depth
+                frames = pipeline.wait_for_frames()
+
+                # Get depth frame
+                depth_frame = frames.get_depth_frame()
+
+                # Perform filtering
+                filtered = dec_filter.process(depth_frame)
+
+                filtered = spat_filter.process(filtered)
+                
+                filtered = fill_filter.process(filtered)
+                
+                filtered = temp_filter.process(filtered)
+                
+                # Colorize depth frame to jet colormap
+                depth_color_frame = rs.colorizer().colorize(depth_frame)
+                depth_color_frame_filt = rs.colorizer().colorize(filtered)
+
+                # Convert depth_frame to numpy array to render image in opencv
+                depth_color_image = np.asanyarray(depth_color_frame.get_data())
+                depth_color_image_filt = np.asanyarray(depth_color_frame_filt.get_data())
+                depth_image = np.asanyarray(depth_frame.get_data())
+
+                # Render image in opencv window
+                cv2.imshow("Depth Stream", depth_color_image)
+                cv2.imshow("Depth Stream Filted", depth_color_image_filt)
+
+                key = cv2.waitKey(1)
+                # if pressed escape exit program
+                if key == 27:
+                    cv2.destroyAllWindows()
+                    break
+        finally:
+            pass
+
+    def save_depth_to_npy(self, bagfile_dir, n_img = 3, interval = 100, filter = True):
 
         # -------------------------------------------- #
         # Read the bag file and save depth matrix to .npy file
@@ -96,6 +170,17 @@ class depth_worker():
             # Configure the pipeline to stream the depth stream
             config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
 
+            # Declare filters
+            dec_filter = rs.decimation_filter()   # Decimation - reduces depth frame density
+            
+            spat_filter = rs.spatial_filter()          # Spatial    - edge-preserving spatial smoothing
+            
+            temp_filter = rs.temporal_filter()    # Temporal   - reduces temporal noise
+
+            fill_filter = rs.hole_filling_filter()  # Hole Filling filter
+
+            disp_filter = rs.disparity_transform()  # Disparity Transform filter
+
             # Start streaming from file
             pipeline.start(config)
 
@@ -104,6 +189,9 @@ class depth_worker():
             for i in range((n_img - 1) * interval + 1):
 
                 if (i % interval != 0):
+
+                    # Get frameset of depth
+                    frames = pipeline.wait_for_frames()
 
                     continue
 
@@ -115,8 +203,15 @@ class depth_worker():
                     # Get depth frame
                     depth_frame = frames.get_depth_frame()
 
-                    # Colorize depth frame to jet colormap
-                    # depth_color_frame = rs.colorizer().colorize(depth_frame)
+                    if (filter):
+                        # Perform filtering
+                        filtered = dec_filter.process(depth_frame)
+
+                        filtered = spat_filter.process(filtered)
+
+                        filtered = fill_filter.process(filtered)
+
+                        depth_frame = temp_filter.process(filtered)
                     
                     # Convert depth_frame to numpy array to render image in opencv
                     raw = depth_frame.get_data()
@@ -138,6 +233,8 @@ if __name__ == "__main__":
     
     w = depth_worker()
 
+    #w.play_depth_stream("./20181011_223353.bag")
+    
     w.save_depth_to_npy("./20181011_223353.bag")
 
     w.verify_depth_matrix()
