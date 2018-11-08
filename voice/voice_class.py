76ca897522b993adb8f4440a4955f752640421d8
ShidongS <shidongs@bu.edu>
2018-11-08 16:09:36 -0500

    update voice class and wrapper

--- voice/voice_class.py
+++ voice/voice_class.py
@@ -19,6 +19,8 @@ class VoiceInterface(object):
     self.STOP_file=STOP_file
     self.noway_file=noway_file
 
+    self.prev_path = None
+
   def play(self, pat, width):
     b=10
     center=width/2-0.5
@@ -68,7 +70,32 @@ class VoiceInterface(object):
         if step == 0:
             play(self.straight_file)
     play(self.STOP_file)
-        
+
+  def play2(self, pat, width):
+    center=width/2-0.5
+    if len(pat)==0:
+        play(self.noway_file)
+        self.prev_path=10
+        return
+    path=[]
+    path.append(pat[0]-center)
+    for i in range (1,len(pat)):
+        path.append(pat[i]-pat[i-1])
+    print(path)
+    step=path[0]
+    if step == -1 and step!=self.prev_path:
+        play(self.turnleft_file)
+    if step == -2 and step!=self.prev_path:
+        play(self.hardleft_file)
+    if step == 1 and step!=self.prev_path:
+        play(self.turnright_file)
+    if step == 2 and step!=self.prev_path:
+        play(self.hardright_file)
+    if step == 0 and step!=self.prev_path:
+        play(self.straight_file)
+
+    self.prev_path = step
+
 if __name__=="__main__":
 
     interface = VoiceInterface()
