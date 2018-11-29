8f80f84bcd0627e65dc06b2be4f3e49fa6c69ce8
Emily Stern <emistern@bu.edu>
2018-11-29 16:29:15 -0500

    Added second graph to plot individual times

--- path_planning/plot_path_planner_test.py
+++ path_planning/plot_path_planner_test.py
@@ -14,25 +14,55 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 # evenly sampled time at 200ms intervals
-t = np.arange(0., 5., 0.2)
+x = np.arange(0., 5., 1)
+#super fine -> coarse
+astar_hallway = [0.0083, 0.0108, 0.0378, 0.0574, 0.0933]
+dijkstra_hallway  = [0.015, 0.0208, 0.1357, 0.2343,0.2828]
+astar_sparse = [0.0097, 0.0202, 0.0352, 0.069, 0.1303]
+dijkstra_sparse = [0.0115, 0.0514, 0.0762, 0.2214, 0.4565]
+astar_dense = [0.0076, 0.0165, 0.0368, 0.0683,0.0944]
+dijkstra_dense = [0.0152, 0.0486, 0.118, 0.2127, 0.4231]
 
 # red dashes, blue squares and green triangles
-plt.plot(t, t, 'r--', label='Open Space')
-plt.plot(t, t**2, 'bs', label='Hallway')
-plt.plot(t, t**3, 'g^', label='Sparse Obstacles')
-plt.plot(t, t**4, 'c*', label='Dense Obstacles')
+plt.figure(1)
+plt.plot(x, [.2, .2, .2, .2, .2], 'r--', label='Open Space')
+plt.plot(x, [100*astar/dijk for astar,dijk in zip(astar_hallway,dijkstra_hallway)], 'bs', label='Hallway')
+plt.plot(x, [100*astar/dijk for astar,dijk in zip(astar_sparse,dijkstra_sparse)], 'g^', label='Sparse Obstacles')
+plt.plot(x, [100*astar/dijk for astar,dijk in zip(astar_dense,dijkstra_dense)], 'c*', label='Dense Obstacles')
 plt.legend()
 plt.xlabel('Scale')
-plt.ylabel('Dijkstra/A* (%)')
+plt.xticks(x, ['Super Coarse', 'Coarse', 'Average', 'Fine', 'Super Fine'])
+plt.ylabel('A*/Dijkstra (%)')
 
+
+## Individual Timed Plots
+
+
+
+plt.figure(2)
+plt.plot(x, [.2, .2, .2, .2, .2], 'b--', label='Open Space A')
+plt.plot(x, astar_hallway, 'bs', label='Hallway A')
+plt.plot(x, astar_sparse, 'b^', label='Sparse Obstacles A')
+plt.plot(x, astar_dense, 'b*', label='Dense Obstacles A')
+plt.plot(x, [.2, .2, .2, .2, .2], 'g--', label='Open Space D')
+plt.plot(x, dijkstra_hallway, 'gs', label='Hallway D')
+plt.plot(x, dijkstra_sparse, 'g^', label='Sparse Obstacles D')
+plt.plot(x, dijkstra_dense, 'g*', label='Dense Obstacles D')
+blue_patch = mpatches.Patch(color='blue', label='A*')
+green_patch = mpatches.Patch(color='green', label='Dijkstra')
+plt.legend(handles=[blue_patch, green_patch])
+plt.xlabel('Scale')
+plt.xticks(x, ['Super Coarse', 'Coarse', 'Average', 'Fine', 'Super Fine'])
+plt.ylabel('Planning Time (s)')
 ########### BAR GRAPH #############
  
 # data to plot
 n_groups = 4
-means_frank = (90, 55, 40, 65)
-means_guido = (85, 62, 54, 20)
+means_astar = (0.0933, 0.0574, 0.0378, .02)
+means_dijkstra = (0.2828, 0.2343, 0.1357, .09)
  
 # create plot
 
@@ -42,12 +72,12 @@ bar_width = 0.35
 opacity = 0.8
 
 
-rects1 = plt.bar(index, means_frank, bar_width,
+rects1 = plt.bar(index, means_astar, bar_width,
                  alpha=opacity,
                  color='b',
                  label='A*')
  
-rects2 = plt.bar(index + bar_width, means_guido, bar_width,
+rects2 = plt.bar(index + bar_width, means_dijkstra, bar_width,
                  alpha=opacity,
                  color='g',
                  label='Dijkstra')
