353aedb097ac1247f9e6d9f51bb9d8176342b1a3
yxiao1996 <xiaoyu960522@sina.com>
2018-12-10 17:11:19 -0500

    get the gui,oy back;

--- path_planning/lapanalysis_dev.py
+++ path_planning/lapanalysis_dev.py
@@ -1,5 +1,7 @@
 import numpy as np
 import time
+from draw import draw_max_conn
+import cv2
 
 def compute_adjacency_degree(grid_map):
 
@@ -64,11 +66,53 @@ def compute_eig(lap):
 
     print(np.sort(eig_vals))
 
+def reorder_laplacian(lap):
+
+    # reorder the graph laplacian
+    diag_blocks = [[0, 1, 2]]
+
+    mtx_size = lap.shape[0]
+
+    for i in range(1, mtx_size):
+        # loop through all the nodes
+        if (lap[i, i] == 0):
+            # a node with degree 0
+            diag_blocks.append([i])
+        else:
+            in_block = False
+            for j in range(i):
+                # loop through possible connections
+                for k, block in enumerate(diag_blocks):
+                    if (lap[j, i] == -1) and (j in block) and (i not in block):
+                        block.append(i)
+                        diag_blocks[k] = block
+                        in_block = True
+                        break
+                #if (in_block):
+                #    break
+            if (not in_block):
+                diag_blocks.append([i])
+
+    return diag_blocks
+
+def pick_laplacian(lap, idx):
+    print(idx)
+    pick_idx = np.array(idx[0])
+
+    pick_lap = np.take(lap, pick_idx, axis=0)
+    pick_lap = np.take(pick_lap, pick_idx, axis=1)
+
+    for i in range(pick_lap.shape[0]):
+        #print(np.sum(pick_lap[i], axis=0))
+        pick_lap[i, i] -= np.sum(pick_lap[i], axis=0)
+
+    return pick_lap
+
 if __name__ == "__main__":
 
     debug_map = [
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 1., 0., 0., 0., 0., 0.],
+        [1., 1., 1., 0., 0., 0., 1., 1., 1.],
+        [0., 0., 1., 1., 0., 0., 0., 0., 0.],
         [0., 0., 1., 1., 0., 0., 0., 0., 0.],
         [0., 0., 1., 0., 0., 0., 1., 0., 0.],
         [0., 1., 1., 0., 0., 1., 1., 0., 0.],
@@ -76,29 +120,40 @@ if __name__ == "__main__":
         [0., 1., 1., 0., 0., 0., 0., 0., 0.],
         [0., 0., 0., 0., 1., 0., 0., 0., 0.],
         [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.],
-        [0., 0., 0., 0., 0., 0., 0., 0., 0.]
+        [1., 1., 1., 1., 1., 1., 1., 1., 1.],
+        [0., 0., 0., 0., 0., 0., 1., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 1., 0., 0.]
         ]
 
     big_map = [
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1],
+        [1, 1, 0, 1, 1],
         [0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0]]
+        [0, 0, 0, 0, 0]]
 
     sml_map = [
-        [0, 0, 0],
+        [0, 1, 0],
         [0, 1, 1],
         [0, 0, 0]
     ]
 
+    test_map = debug_map
+
     st = time.time()
-    adj, dgr = compute_adjacency_degree(big_map)
+    adj, dgr = compute_adjacency_degree(test_map)
 
     lap = compute_laplacian(adj, dgr)
 
-    compute_eig(lap)
+    diag_idx = reorder_laplacian(lap)
+
+    sml_lap = pick_laplacian(lap, diag_idx)
+
+    print(sml_lap)
+    #compute_eig(sml_lap)
     ed = time.time()
     print("in time: ", ed-st)
+
+    world = draw_max_conn(test_map, diag_idx[0])
+    cv2.imshow("max conn", world)
+    cv2.waitKey(0)
